942776693d5d4d0ea54c4b8378433d51efff3948
amadecode <amadecode@gmail.com>
2017-11-22 23:17:44 -0800

    heroku first deployment

---
+++ config/keys.js
@@ -0,0 +1,27 @@
+module.exports = {
+	google: {
+		clientID: '601024863834-qgt4a4oi9ebnou84d4285e5j3niapfe3.apps.googleusercontent.com',
+		clientSecret: 'IMpFYJX1xYCQAxq6_DEsao6r'
+	},
+	mongodb: {
+		dbURI: 'mongodb://hfmp:hfmp@ds117136.mlab.com:17136/hfmp'
+	},
+	session: {
+		cookieKey: 'thisisasecretkey'
+	},
+	nodemailer: {
+		user: 'cbalamanatuic@gmail.com',
+		pass: 'Ch3st3rarian3092986'
+	}
+};
+
+/*
+	Ethreal
+	auth:{
+		user: 'v7vdnwwrknnirc76@ethereal.email',
+		pass: '9etJNJdRXqxgneVupw'
+	}
+
+	NodeMailer + Gmail
+	https://medium.com/@manojsinghnegi/sending-an-email-using-nodemailer-gmail-7cfa0712a799
+*/
